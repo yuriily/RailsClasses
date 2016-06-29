2315cc58fae4f9770f5031d67d19b19e98ebc392
Yuriy Lytovchenko <yurii.lytovchenko@playtech.com>
2016-06-29 18:38:01 +0300

    added writing to csv

--- src/main/java/FetchData.java
+++ src/main/java/FetchData.java
@@ -13,84 +13,107 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class FetchData {
-	public static final String APIKEY = "BtiEB.ufbdTcx.yGGVb/-sXfC1RvxnCwcaeecZQby"; 
+	public static final String APIKEY = "BtiEB.ufbdTcx.yGGVb/-sXfC1RvxnCwcaeecZQby";
 	public static final int PROJECT_ID = 31;
 	public static final int FULL_TEST_SUITE_ID = 301;
 	public static final int CONFIG_ID = 124;
+	public static final String fileNameForCSV = "C:\\Trash\\~tmp\\rails.csv";
 	
-
+	public ConfigurationItem configItems[]; 
+	
+	private LinkedHashSet<String> dataRows;
+	private LinkedHashSet<String> dataCols;
 
 	public static void main(String[] args) throws MalformedURLException, IOException, APIException {
 		// TODO Auto-generated method stub
 		System.out.println("Started");
-		
+
 		trustEveryone();
-		
+		Gson gson = new Gson();
+
 		APIClient client = new APIClient("https://testrail.ee.playtech.corp/testrail/");
 		client.setUser("yuriily");
 		client.setPassword("Artalaire543");
-		
-		Gson gson = new Gson();
+
 
 		System.out.println("Getting projects...");
 		JSONArray jsonProjects = (JSONArray) client.sendGet("get_projects");
 		Project projects[] = gson.fromJson(jsonProjects.toJSONString(), Project[].class);
-		
+
 		System.out.println("Getting suites...");
-		JSONArray jsonSuites = (JSONArray) client.sendGet("get_suites/"+PROJECT_ID);
+		JSONArray jsonSuites = (JSONArray) client.sendGet("get_suites/" + PROJECT_ID);
 		Suite suites[] = gson.fromJson(jsonSuites.toJSONString(), Suite[].class);
-		
+
 		System.out.println("Getting cases...");
-		JSONArray jsonCases = (JSONArray) client.sendGet("get_cases/"+PROJECT_ID+ "/&suite_id=" + FULL_TEST_SUITE_ID);
+		JSONArray jsonCases = (JSONArray) client
+				.sendGet("get_cases/" + PROJECT_ID + "/&suite_id=" + FULL_TEST_SUITE_ID);
 		Case cases[] = gson.fromJson(jsonCases.toJSONString(), Case[].class);
-		
-		System.out.println("Getting configurations...");
-		JSONArray jsonConfigs = (JSONArray) client.sendGet("get_configs/"+PROJECT_ID);
-		Configuration configs[] = gson.fromJson(jsonConfigs.toJSONString(), Configuration[].class);
-		ArrayList<ConfigurationItem> configItems=null;
-		for(int iter=0;iter<configs.length;iter++) {
-			System.out.println(configs[iter].getId()+":"+configs[iter].getName());
-			if(CONFIG_ID == configs[iter].getId())
-				configItems= new ArrayList<ConfigurationItem>(Arrays.asList(configs[iter].getConfigurationItems()));
+		LinkedHashSet<String> dataRows = new LinkedHashSet<String>();
+		//loop here to check for nulls
+		for(int iter=0; iter<cases.length; iter++) {
+			if(!cases[iter].getTitle().equals(""))
+				dataRows.add(cases[iter].getTitle());
 		}
+
+		System.out.println("Getting configurations...");
+		JSONArray jsonConfigs = (JSONArray) client.sendGet("get_configs/" + PROJECT_ID);
+		Configuration configs[] = gson.fromJson(jsonConfigs.toJSONString(), Configuration[].class);
+		LinkedHashSet<String> dataCols = new LinkedHashSet<String>();
+
+		System.out.println("Configuration items fill-in...");
+		for (int iter = 0; iter < configs.length; iter++) {
+			if (CONFIG_ID == configs[iter].getId()) {
+				ConfigurationItem configItems[] = Arrays.copyOf(configs[iter].getConfigurationItems(), 
+						configs[iter].getConfigurationItems().length);
+				for (int configIter = 0; configIter < configItems.length; configIter++)
+					dataCols.add(configItems[configIter].getName());
+			}
+		}
+		ValuesMatrix values = new ValuesMatrix(dataRows, dataCols);
+		System.out.println("Our table:");
+		System.out.println(values.toPrettyString());
 		
-		System.out.println("Configuration items:");
-		for(int iter=0;iter<configItems.size();iter++)
-			System.out.println(configItems.get(iter).getId()+":"+configItems.get(iter).getName());
-		
-		
+		try {
+			values.writeToCSV(fileNameForCSV);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 	
+	
+
 	private static void trustEveryone() {
-        try {
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-            SSLContext context = SSLContext.getInstance("TLS");
-            context.init(null, new X509TrustManager[]{new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
+		try {
+			HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+			SSLContext context = SSLContext.getInstance("TLS");
+			context.init(null, new X509TrustManager[] { new X509TrustManager() {
+				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
 
-                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
+				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
 
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            }}, new SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
-        } catch (Exception e) { // should never happen
-            e.printStackTrace();
-        }
-    }
+				public X509Certificate[] getAcceptedIssuers() {
+					return new X509Certificate[0];
+				}
+			} }, new SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+		} catch (Exception e) { // should never happen
+			e.printStackTrace();
+		}
+	}
 
 }
---
+++ src/main/java/ValuesMatrix.java
@@ -0,0 +1,131 @@
+
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.supercsv.cellprocessor.ParseBool;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvMapWriter;
+import org.supercsv.io.ICsvMapWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import javafx.util.Pair;
+
+public class ValuesMatrix {
+	private static final String FIRST_COLUMN_NAME = "Test name";
+	
+	private LinkedHashSet<String> dataRows;
+	private LinkedHashSet<String> dataCols;
+	private Map<String, Map<String, ?>> isIncluded;
+	
+	
+	public ValuesMatrix(LinkedHashSet<String> dataRows, LinkedHashSet<String> dataCols) {
+		super();
+		this.dataRows = dataRows;
+		this.dataCols = new LinkedHashSet<String>();
+		this.dataCols.add(FIRST_COLUMN_NAME);
+		this.dataCols.addAll(dataCols);
+		Iterator<String> iterRows = dataRows.iterator();
+		Iterator<String> iterCols = dataCols.iterator();
+		this.isIncluded=new HashMap<String, Map<String,?>>();
+		
+		//fill in with false, as this constructor is for creating a template 
+		//all the cells of the matrix will be empty
+		while(iterRows.hasNext()) {
+			String currentRow = (String)iterRows.next();
+			HashMap<String, Boolean> currentRowValues = new HashMap<String,Boolean>();
+			iterCols = dataCols.iterator();
+			while(iterCols.hasNext())
+				currentRowValues.put(iterCols.next(), Boolean.FALSE);
+			this.isIncluded.put(currentRow, currentRowValues);
+				
+		}
+		
+	}
+	public ValuesMatrix() {
+		// TODO maybe delete it as empty object is not usable
+	}
+	
+	
+	public LinkedHashSet<String> getDataRows() {
+		return dataRows;
+	}
+	public void setDataRows(LinkedHashSet<String> dataRows) {
+		this.dataRows = dataRows;
+	}
+	public LinkedHashSet<String> getDataCols() {
+		return dataCols;
+	}
+	public void setDataCols(LinkedHashSet<String> dataCols) {
+		this.dataCols = dataCols;
+	}
+	
+	public String toPrettyString() {
+		//output as a table
+		
+		StringBuilder sb = new StringBuilder();
+		//header row
+		Iterator<String> iterRows = dataRows.iterator();
+		Iterator<String> iterCols = dataCols.iterator();
+		while(iterCols.hasNext())
+			sb.append(iterCols.next() + ",");
+		sb.deleteCharAt(sb.length()-1);
+		sb.append(System.getProperty("line.separator"));
+		
+		//all other rows
+		while(iterRows.hasNext()) {
+			String currentRowHeader = iterRows.next();
+			iterCols = this.dataCols.iterator();
+			sb.append(currentRowHeader+",");
+			while(iterCols.hasNext())
+				sb.append(this.isIncluded.get(currentRowHeader).get(iterCols.next())+",");
+			sb.deleteCharAt(sb.length()-1);
+			sb.append(System.getProperty("line.separator"));
+		}
+		
+		return sb.toString();
+	}
+	
+	public void writeToCSV(String fileName) throws IOException {
+		//writes its contents to csv located in provided place
+		
+		ICsvMapWriter mapWriter = null;
+		
+		//create header for the table
+		final String[] header = new String[getDataCols().size()];
+		getDataCols().toArray(header);
+		
+		//create cell processor now, when we know the number of columns
+		//first column is always string, other ones for now are always boolean
+		final CellProcessor[] processors = new CellProcessor[header.length];
+//		processors[0] = new NotNull();
+//		for(int iter=1; iter<header.length; iter++)
+//			processors[iter] = new NotNull(new ParseBool());
+
+		try {
+		//try to create a file now
+		mapWriter = new CsvMapWriter(new FileWriter(fileName), CsvPreference.STANDARD_PREFERENCE);
+		//write the file map-by-map
+		mapWriter.writeHeader(header);
+		for(Entry<String, Map<String, ?>> entry : isIncluded.entrySet()) {
+			HashMap<String, Object> oneRow = (HashMap<String, Object>) entry.getValue();
+			oneRow.put(FIRST_COLUMN_NAME, entry.getKey());
+			mapWriter.write(oneRow, header, processors);
+		}
+		}
+		finally {
+			if(null!=mapWriter) {
+				mapWriter.close();
+			}
+		}
+		
+	}
+
+}
